d3e4c67d83e4c33e4a881a6a7c77c56392942dd5
jason-guo-cr <guojson@gmail.com>
2023-11-20 23:36:28 +0800

    feat: Oauth配置枚举（未完，暂时用于占位）

---
+++ only-oauth-core/src/main/java/com/maingirder/oauth/core/enums/OAuthPlatformEnum.java
@@ -0,0 +1,12 @@
+package com.maingirder.oauth.core.enums;
+
+/**
+ * oauth platform general config
+ *
+ * @author jason-guo-cr
+ * @version 1.0.0
+ * @since 2023/11/20 22:00
+ */
+public enum OAuthPlatformEnum {
+
+}
